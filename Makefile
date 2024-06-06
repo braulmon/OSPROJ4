0021b6e2770c8548b490fc5b0185f1593d0bc24d
braulmon <brauliomontelongo1@gmail.com>
2024-06-05 23:01:26 -0700

    Phase I complete

--- Makefile
+++ Makefile
@@ -1,16 +1,16 @@
 CC = gcc
 CFLAGS = -Wall -g
-PROG = tinyFSDemo
-OBJS = tinyFSDemo.o libTinyFS.o libDisk.o
+PROG = diskTest
+OBJS = diskTest.o libDisk.o
 
 $(PROG): $(OBJS)
 	$(CC) $(CFLAGS) -o $(PROG) $(OBJS)
 
-tinyFsDemo.o: tinyFSDemo.c libTinyFS.h tinyFS.h TinyFS_errno.h
-	$(CC) $(CFLAGS) -c -o $@ $<
+diskTest.o: diskTest.c libDisk.h
+	$(CC) $(CFLAGS) -c diskTest.c
 
-libTinyFS.o: libTinyFS.c libTinyFS.h tinyFS.h libDisk.h libDisk.o TinyFS_errno.h
-	$(CC) $(CFLAGS) -c -o $@ $<
+libDisk.o: libDisk.c libDisk.h
+	$(CC) $(CFLAGS) -c libDisk.c
 
-libDisk.o: libDisk.c libDisk.h tinyFS.h TinyFS_errno.h
-	$(CC) $(CFLAGS) -c -o $@ $<
+clean:
+	rm -f $(PROG) $(OBJS)
---
+++ libDisk.c
@@ -0,0 +1,74 @@
+#include "libDisk.h"
+#include <unistd.h>
+#include <fcntl.h>
+
+int openDisk(char *filename, int nBytes) {
+    int new_nBytes;
+    int disk = -1; // default file open failure value
+
+    if (nBytes < BLOCKSIZE && nBytes != 0) {
+        return -1;
+    } else if (nBytes % BLOCKSIZE != 0) {
+        new_nBytes = (nBytes / BLOCKSIZE) * BLOCKSIZE; // closest multiple to BLOCKSIZE < nBytes
+    } else {
+        new_nBytes = nBytes;
+    }
+
+    // open disk (unix file)
+    if (nBytes == 0) { // disk already exists
+        disk = open(filename, O_RDWR, 0666);
+    } else {
+        disk = open(filename, O_RDWR | O_CREAT, 0666);
+    }
+
+    if (disk < 0) {
+        return -1; // file open failure
+    }
+    
+    // if new_nBytes is set, truncate the file
+    if (new_nBytes != 0) {
+        int sizeSet = ftruncate(disk, new_nBytes);
+        if (sizeSet != 0) {
+            closeDisk(disk);
+            return -1;
+        }
+    }
+
+    return disk;
+}
+
+int closeDisk(int disk) {
+    return close(disk);
+}
+
+int readBlock(int disk, int bNum, void *block) {
+    off_t offset = bNum * BLOCKSIZE;
+    off_t diskOffset = lseek(disk, offset, SEEK_SET);
+
+    if (diskOffset < 0) {
+        return -1;
+    }
+
+    ssize_t bytesRead = read(disk, block, BLOCKSIZE);
+    if (bytesRead < 0 || bytesRead != BLOCKSIZE) {
+        return -1; // unable to read
+    }
+
+    return 0; // read
+}
+
+int writeBlock(int disk, int bNum, void *block) {
+    off_t blockOffset = bNum * BLOCKSIZE;
+    off_t diskOffset = lseek(disk, blockOffset, SEEK_SET);
+
+    if (diskOffset < 0) {
+        return -1;
+    }
+
+    ssize_t bytesWritten = write(disk, block, BLOCKSIZE);
+    if (bytesWritten != BLOCKSIZE) {
+        return -1; // unable to write
+    }
+
+    return 0; // written
+}
